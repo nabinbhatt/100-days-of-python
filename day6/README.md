25736577ab0c3c051850997598cdfac47982fadb
Nabin Bhatt <83168881+nabinbhatt@users.noreply.github.com>
2022-07-29 00:04:10 +0545

    day6 complete

---
+++ day6/README.md
@@ -0,0 +1,5 @@
+# Day 6 | #100DaysOfCode
+Today, I learned about:
+- Defining and Calling Python Functions
+- Indentation in Python
+- While loops
---
+++ day6/while-loop.py
@@ -0,0 +1,13 @@
+# Print i as long as i is less than 6:
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+# defining new functions
+def my_function():
+  print("Hello")
+  print("Bye")
+
+# calling function 
+my_function()
