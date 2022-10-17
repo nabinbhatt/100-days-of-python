8be281483a7b1286704e9ba8e8c54b2348df1e6b
Nabin Bhatt <83168881+nabinbhatt@users.noreply.github.com>
2022-10-17 21:16:26 +0545

    minor fix

--- day10/calculator/main.py
+++ day10/calculator/main.py
@@ -34,7 +34,7 @@ def calculator():
 
         print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-        if input("Type 'y' to continue calculation with {answer}, or type 'n' to start a new calculation: ") == "y":
+        if input(f"Type 'y' to continue calculation with {answer}, or type 'n' to start a new calculation: ") == "y":
             num1 = answer
         else:
             should_continue = False
