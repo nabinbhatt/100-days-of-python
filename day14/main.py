54ab27b21789f3de9267d4a18b06929f0d9c31e8
Nabin Bhatt <83168881+nabinbhatt@users.noreply.github.com>
2022-10-21 00:14:59 +0545

    update main.py

--- day14/main.py
+++ day14/main.py
@@ -1,4 +1,4 @@
-# a game of higher or lower using instagram followers
+
 import art
 from replit import clear
 from game_data import data
@@ -6,9 +6,9 @@ import random
 
 def format_data(account):
   """format the account data into printable format."""
-  account_name = account_a["name"]
-  account_descr = account_a["description"]
-  account_country = account_a["country"]
+  account_name = account["name"]
+  account_descr = account["description"]
+  account_country = account["country"]
   return f"{account_name}, a {account_descr}, from {account_country}"
 
 def check_answer(guess, a_followers, b_followers):
@@ -17,45 +17,48 @@ def check_answer(guess, a_followers, b_followers):
     return guess == "a"
   else:
     return guess == "b"
-  
-# display art 
-print(art.logo)
-score = 0
-
-# make the game repeatable.
-game_should_continue = True
-# making account at position B become the next account at position A
-account_b = random.choice(data)
-while game_should_continue:
-  # generate a random account from the game data
-  account_a = account_b
-  account_b = random.choice(data)
-  while account_a == account_b:
-    account_b = random.choice(data)
-  
-  # format the account data into printable format.
-  print(f"Compare A: {format_data(account_a)}.")
-  print(art.vs)
-  print(f"Against B: {format_data(account_b)}.")
-  
-  # ask user for a guess.
-  guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-  
-  # check if user is correct.
-  ## get follower count of each account.
-  a_follower_count = account_a["follower_count"]
-  b_follower_count = account_b["follower_count"]
-  ## use if statement to chek if user is correct.
-  is_correct = check_answer(guess, a_follower_count, b_follower_count)
-  # clear the screen
-  clear()
+def game():  
+  # display art 
   print(art.logo)
+  score = 0
   
-  # give user feedback on their guess
-  if is_correct:
-    # score keeping
-    score += 1
-    print(f"You're right! Current score: {score}.")
-  else:
-    game_should_continue = False
-    print(f"Sorry, that's wrong, Final score: {score}.")
+  # make the game repeatable.
+  game_should_continue = True
+  # making account at position B become the next account at position A
+  account_a = random.choice(data)
+  account_b = random.choice(data)
+  while game_should_continue:
+    # generate a random account from the game data
+    account_a = account_b
+    account_b = random.choice(data)
+    while account_a == account_b:
+      account_b = random.choice(data)
+    
+    # format the account data into printable format.
+    print(f"Compare A: {format_data(account_a)}.")
+    print(art.vs)
+    print(f"Against B: {format_data(account_b)}.")
+    
+    # ask user for a guess.
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    
+    # check if user is correct.
+    ## get follower count of each account.
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    ## use if statement to chek if user is correct.
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+    # clear the screen
+    clear()
+    print(art.logo)
+    
+    # give user feedback on their guess
+    if is_correct:
+      # score keeping
+      score += 1
+      print(f"You're right! Current score: {score}.")
+    else:
+      game_should_continue = False
+      print(f"Sorry, that's wrong, Final score: {score}.")
+
+game()
